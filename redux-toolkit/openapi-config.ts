6b7fb0b607a25d9fe5bdd5d1534b81707f36a50d
Elias Schablowski <eschablowski@me.com>
2022-11-17 17:09:27 +0000

    Add copy to build
    Signed-off-by: Elias Schablowski <eschablowski@me.com>

--- redux-toolkit/openapi-config.ts
+++ redux-toolkit/openapi-config.ts
@@ -2,7 +2,7 @@ import type { ConfigFile } from "@rtk-query/codegen-openapi";
 
 const config: ConfigFile = {
     schemaFile: "../openapi.yaml",
-    apiFile: "../build/ts/emptyApi.ts",
+    apiFile: "../build/ts/emptyAPI.ts",
     apiImport: "emptySplitApi",
     outputFile: "../build/ts/redux.ts",
     exportName: "ridesaverAPI",
