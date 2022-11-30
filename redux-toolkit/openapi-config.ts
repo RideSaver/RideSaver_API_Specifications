923365eb1fdb7ae6fcf45f4928bf9db1785212f2
Elias Schablowski <eschablowski@me.com>
2022-11-29 23:30:58 -0800

    Fix ts import
    Signed-off-by: Elias Schablowski <eschablowski@me.com>

--- redux-toolkit/openapi-config.ts
+++ redux-toolkit/openapi-config.ts
@@ -2,7 +2,7 @@ import type { ConfigFile } from "@rtk-query/codegen-openapi";
 
 const config: ConfigFile = {
     schemaFile: "../openapi.yaml",
-    apiFile: "../build/ts/emptyAPI.ts",
+    apiFile: "../build/ts/emptyApi.ts",
     apiImport: "emptySplitApi",
     outputFile: "../build/ts/redux.ts",
     exportName: "ridesaverAPI",
